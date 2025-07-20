a307a1261a08c419993718b032adade12114484f
KingRavo18 <rkingovskis14@gmail.com>
2025-07-20 22:40:59 +0300

    jrt

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -9,16 +9,36 @@ export default function Header(){
         <header>
             <nav className="float-left">
                 <ul>
-                    <li><button className="text-[150%]">LV</button></li>
-                    <li><button className="text-[150%]">EN</button></li>
+                    <li>
+                        <button className="text-[150%]">LV</button>
+                    </li>
+                    <li>
+                        <button className="text-[150%]">EN</button>
+                    </li>
                 </ul>
             </nav>
             <nav className="Navigation ml-[60%]">
                 <ul>
-                    <li><a href="#contact"><button>{t.contact}</button></a></li>
-                    <li><a href="#projects"><button>{t.projects}</button></a></li>
-                    <li><a href="#experiance"><button>{t.experience}</button></a></li>
-                    <li><a href="#introduction"><button>{t.introduction}</button></a></li>
+                    <li>
+                        <a href="#contact">
+                            <button>{t.contact}</button>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#projects">
+                            <button>{t.projects}</button>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#experiance">
+                            <button>{t.experience}</button>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#introduction">
+                            <button>{t.introduction}</button>
+                        </a>
+                    </li>
                 </ul>
             </nav>
         </header>
